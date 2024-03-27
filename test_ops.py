1e33b37a63e5e7a170e4d9fc9e9efbd60dffbb20
Sviatoslav Kurdydyk <skurdydy@cisco.com>
2024-03-27 22:41:08 +0200

    Update test_ops.py

--- test_ops.py
+++ test_ops.py
@@ -1,47 +1,17 @@
-def add(x, y):
-    """Function to add two numbers.
+from ops import *
 
-    Parameters:
-    x (int): The first number.
-    y (int): The second number.
 
-    Returns:
-    int: The sum of the two numbers.
-    """
-    return x + y
+def test_add():
+    assert add(2,3) == 5
 
-def subtract(x, y):
-    """Function to subtract one number from another.
 
-    Parameters:
-    x (int): The first number.
-    y (int): The number to subtract from the first number.
+def test_subtract():
+    assert subtract(2, 3) == -1
 
-    Returns:
-    int: The difference between the first and second numbers.
-    """
-    return x - y
 
-def multiply(x, y):
-    """Function to multiply two numbers.
+def test_multiply():
+    assert multiply(2, 3) == 6
 
-    Parameters:
-    x (int): The first number.
-    y (int): The second number.
 
-    Returns:
-    int: The product of the two numbers.
-    """
-    return x * y
-
-def divide(x, y):
-    """Function to divide one number by another.
-
-    Parameters:
-    x (int): The dividend.
-    y (int): The divisor.
-
-    Returns:
-    float: The result of dividing the dividend by the divisor.
-    """
-    return x / y
+def test_divide():
+    assert divide(10,5) == 2
